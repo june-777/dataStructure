7a49e2ffe3fd1a27c8ac16ba311ec74bd88f5e78
June <68291395+june-777@users.noreply.github.com>
2022-04-23 16:31:41 +0900

    Exception Throw를 위한 클래스

---
+++ 자료구조/Queue/arrayQueue/EmptyQueueException.java
@@ -0,0 +1,10 @@
+package arrayQueue;
+
+public class EmptyQueueException extends Exception{
+	public EmptyQueueException() {
+		// TODO Auto-generated constructor stub
+	}
+	public EmptyQueueException(String message) {
+		super(message);
+	}
+}
---
+++ 자료구조/Queue/arrayQueue/FullQueueException.java
@@ -0,0 +1,13 @@
+package arrayQueue;
+
+@SuppressWarnings("serial")
+public class FullQueueException extends Exception {
+
+	public FullQueueException() {
+		// TODO Auto-generated constructor stub
+	}
+	public FullQueueException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+}
